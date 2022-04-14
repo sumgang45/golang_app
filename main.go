89dcaf2b2bf8765152dd698f9fcf27e661a8e30a
Ganga Sumanth <ganga.sumanth@we45.com>
2022-04-14 14:10:25 +0530

    inital commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "home.tmpl", nil)
+	})
+
+	router.Run(":" + port)
+}
