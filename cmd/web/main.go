07c2e90f92353213fccb2804eda87272780e5596
Ganga Sumanth <ganga.sumanth@we45.com>
2022-05-01 17:32:20 +0530

    refactored code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/sumgang45/LearningGo/webapp/pkg/handlers"
 )
 
 const portNumber = ":8080"
 
 //this is the main application function
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port: %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -2,13 +2,15 @@ package handlers
 
 import (
 	"net/http"
+
+	"github.com/sumgang45/LearningGo/webapp/pkg/render"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 //this is an about page and it also receives information from the addvalues private func
 func About(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
