983cff9c2236a687392ceddcf544e742aaa90da4
Ganga Sumanth <ganga.sumanth@we45.com>
2022-05-01 21:10:09 +0530

    built a template cache (map) and returned errors

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -3,8 +3,11 @@ package render
 import (
 	"fmt"
 	"net/http"
+	"path/filepath"
 	"text/template"
 )
+
+var functions = template.FuncMap{}
 
 //renders templares using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
@@ -16,4 +19,38 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 		fmt.Println("Error parsing template:", err)
 		return
 	}
+}
+
+func RenderTemplateTest(w http.ResponseWriter, tmpl string) (map[string]*template.Template, error) {
+	//map: a variable to look up objects very quickly. It is a cache to look up all templates and then render the appropriate per req
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob(".templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[name] = ts
+	}
+
+	return myCache, nil
 }
