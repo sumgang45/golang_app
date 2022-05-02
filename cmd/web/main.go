4848c3c4db3ebb9071b9f8df5c30943fa53d8c33
Ganga Sumanth <ganga.sumanth@we45.com>
2022-05-02 08:27:47 +0530

    session management

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/sumgang45/LearningGo/webapp/pkg/config"
 	"github.com/sumgang45/LearningGo/webapp/pkg/handlers"
 	"github.com/sumgang45/LearningGo/webapp/pkg/render"
@@ -12,9 +14,21 @@ import (
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var sessionManager *scs.SessionManager
+
 //this is the main application function
 func main() {
-	var app config.AppConfig
+	//change this to true when in production
+	app.InProduction = false
+
+	sessionManager = scs.New()
+	sessionManager.Lifetime = 24 * time.Hour
+	sessionManager.Cookie.Persist = true
+	sessionManager.Cookie.SameSite = http.SameSiteLaxMode
+	sessionManager.Cookie.Secure = app.InProduction
+
+	app.SessionManager = sessionManager
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,11 +3,15 @@ package config
 import (
 	"html/template"
 	"log"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 //holds application config
 type AppConfig struct {
-	UseCache      bool
-	TemplateCache map[string]*template.Template
-	InfoLog       *log.Logger
+	UseCache       bool
+	TemplateCache  map[string]*template.Template
+	InfoLog        *log.Logger
+	InProduction   bool
+	SessionManager *scs.SessionManager
 }
